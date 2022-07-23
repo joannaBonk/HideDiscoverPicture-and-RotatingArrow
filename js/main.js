316723d4a2d07ebf6e66c61d7bcf784f07613361
Joanna <joanna.bonk08@gmail.com>
2022-07-23 16:15:53 +0200

    add rotating arrow

--- js/main.js
+++ js/main.js
@@ -10,7 +10,11 @@ const img = document.querySelector('.item1')
 const showImg = () => {
     img.classList.toggle('hide')
 
-
+if (img.classList.contains('hide')){
+    arrowIcon.style.transform = 'rotate(180deg)'
+} else {
+    arrowIcon.style.transform = 'rotate(0)'
+}
 }
 
 arrowBtn.addEventListener('click', showImg)
